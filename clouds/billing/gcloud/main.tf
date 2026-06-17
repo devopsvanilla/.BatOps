e201901eba0b433c6f3581530996bd0af6bc7e76
Sandro Cicero <devopsvanilla@outlook.com>
2026-06-17 17:28:25 -0300

    feat: add delete_contents_on_destroy option to BigQuery dataset resource

--- clouds/billing/gcloud/main.tf
+++ clouds/billing/gcloud/main.tf
@@ -2,6 +2,7 @@ resource "google_bigquery_dataset" "billing_dataset" {
   dataset_id = var.dataset_id
   project    = var.project_id
   location   = var.location
+  delete_contents_on_destroy = true
 
   description = "Dataset utilizado para exportação de custo detalhado do billing."
   labels = {
